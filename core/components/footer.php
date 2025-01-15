2a91c09d39525727b815df4e24c46f0d53465810
David Paiva <davidsgpaiva@gmail.com>
2025-01-15 18:47:51 +0000

    Add async/await

--- core/components/footer.php
+++ core/components/footer.php
@@ -2,14 +2,14 @@
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 <script>
-    document.addEventListener('DOMContentLoaded', function() {
-        var selectElements = document.querySelectorAll('select');
+    document.addEventListener('DOMContentLoaded', async () => {
+        var selectElements = await document.querySelectorAll('select');
         var selectInstances = M.FormSelect.init(selectElements, {});
 
-        var autocompleteElements = document.querySelectorAll('.autocomplete');
+        var autocompleteElements = await document.querySelectorAll('.autocomplete');
         var autocompleteInstances = M.Autocomplete.init(autocompleteElements, {});
 
-        var modalElements = document.querySelectorAll('.modal');
+        var modalElements = await document.querySelectorAll('.modal');
         var modalInstances = M.Modal.init(modalElements, {});
     });
 </script>
